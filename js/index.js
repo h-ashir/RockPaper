47cc7d196644d6fcf748267aa2c35758b4ab6496
Hashir <Hashiraslam2015@gmail.com>
2024-03-18 14:00:34 +0530

    Complete the UI Feature in the rps branch

--- js/index.js
+++ js/index.js
@@ -1,32 +1,89 @@
-function getComputerChoice(words){
+function getComputerChoice(){
     let randomIndex = Math.floor((Math.random() * words.length));
-    console.log(randomIndex);
+    console.log("Computer: " + words[randomIndex]);
     return words[randomIndex];
 }
 
-function playRound(playerSelection, computerSelection){
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection){
+    const computerSelection = getComputerChoice();
     if (playerSelection === computerSelection){
-        return true;
+        console.log("It's a tie");
+    }
+    else if (
+        (playerSelection == "rock" && computerSelection == "scissors") || 
+        (playerSelection == "scissors" && computerSelection == "paper") || 
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        console.log("You win");
+        ++playerScore;
     }
     else{
-        return false;
+        console.log("Computer win");
+        ++computerScore;
+    }
+    player.textContent = "Player: " + playerScore;
+    computer.textContent = "Computer: " + computerScore;
+    if (playerScore === 5){
+        result.textContent = "Player Wins";
+        result.style.color = "green";
+        playerScore = 0;
+        computerScore = 0;
+        restart.addEventListener("click", () => {
+            result.textContent = "";
+            player.textContent = "Player: " + "0";
+            computer.textContent = "Computer: " + "0";
+        });
+    }
+    else if (computerScore === 5){
+        result.textContent = "Computer Wins";
+        result.style.color = "red";
+        playerScore = 0;
+        computerScore = 0;
+        restart.addEventListener("click", () => {
+            result.textContent = "";
+            player.textContent = "Player: " + "0";
+            computer.textContent = "Computer: " + "0";
+        });
     }
 }
 
-function playGame(times){
-    let words = ["rock", "paper", "scissors"];
-    
-    for (let i=0; i<times; i++){
-        const playerSelection = prompt("Rock paper scissor").toLowerCase();
-        const computerSelection = getComputerChoice(words);
-        let result = playRound(playerSelection, computerSelection);
-        if (result){
-            console.log("Won");
-        }
-        else{
-            console.log("Lose");
-        }   
-    }
-}
-playGame(5);
+let words = ["rock", "paper", "scissors"];
 
+const container = document.querySelector("#container");
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
+
+
+rock.addEventListener("click", function() {
+    playRound("rock");
+});
+paper.addEventListener("click", function() {
+    playRound("paper");
+});
+scissor.addEventListener("click", function() {
+    playRound("scissors");
+});
+
+const div = document.createElement("div");
+div.style.border = "2px solid black";
+div.style.background = "gray";
+div.style.margin = "15px";
+container.appendChild(div);
+
+const player = document.createElement("p");
+const computer = document.createElement("p");
+player.textContent = "Player: " + playerScore;
+computer.textContent = "Computer: " + computerScore;
+div.appendChild(player);
+div.appendChild(computer);
+
+let result = document.createElement("h1");
+result.style.fontSize = "25px"; 
+div.appendChild(result);
+
+const restart = document.querySelector("#restart");
